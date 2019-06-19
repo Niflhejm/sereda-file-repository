02eb67831561b4ad4e045e8bcd2dae1bb45b707d
Hundur <nagafonov91@gmail.com>
2019-06-19 18:08:20 +0300

    add headers

--- src/main/java/ru/sereda/filerepository/seredafilerepository/controllers/FileRepositoryController.java
+++ src/main/java/ru/sereda/filerepository/seredafilerepository/controllers/FileRepositoryController.java
@@ -3,6 +3,7 @@ package ru.sereda.filerepository.seredafilerepository.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,6 @@ import ru.sereda.filerepository.seredafilerepository.services.FileService;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @RestController
@@ -41,14 +41,20 @@ public class FileRepositoryController {
     }
 
     @RequestMapping(path = "/download/{name}", method = RequestMethod.GET)
-    public ResponseEntity<Resource> download(@PathVariable String name) throws FileNotFoundException {
+    public ResponseEntity<Resource> download(@PathVariable String name) throws IOException {
 
         File file = fileService.getFile(name);
         if (file.exists()) {
             MediaType mediaType = fileService.getMediaType(file);
             InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
             return ResponseEntity.ok()
+                    .header("attachment", "filename= +name")
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+name)
+                    .header("Cache-Control", "no-cache, no-store, must-revalidate")
+                    .header("Pragma", "no-cache")
+                    .header("Expires", "0")
                     .contentType(mediaType)
+//                    .contentLength(resource.contentLength())
                     .body(resource);
         } else {
             return ResponseEntity.notFound().header("Reason", "File not found").build();
